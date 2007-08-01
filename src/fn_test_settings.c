9d866f3e3fda626cdf0b896b3867d0c2cdb05cac
Wolfgang Silbermayr <silwol@silwol-laptop>
2007-08-01 19:35:44 +0200

    GPL-ified the freenukum project tree.

--- src/fn_test_settings.c
+++ src/fn_test_settings.c
@@ -1,7 +1,29 @@
 /*******************************************************************
+ *
  * Project: FreeNukum 2D Jump'n Run
  * File:    Tests for Settings functions
- * $Id: fn_test_settings.c 14 2006-07-02 11:02:37Z sttereo3 $
+ *
+ * *****************************************************************
+ *
+ * Copyright 2007 Wolfgang Silbermayr
+ *
+ * *****************************************************************
+ *
+ * This file is part of Freenukum.
+ * 
+ * Freenukum is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  *******************************************************************/
 
 #include <errno.h>
