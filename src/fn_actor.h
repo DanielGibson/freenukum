6f15712bbdf3d0fc731d0e910a638323f7402aaf
Wolfgang Silbermayr <silwol@silwol-laptop>
2008-06-13 10:33:19 +0200

    Removing actor direction because there already exists a horizontal direction enum type.

--- src/fn_actor.h
+++ src/fn_actor.h
@@ -40,13 +40,6 @@ typedef enum fn_actor_type_e fn_actor_type_e;
 
 /* --------------------------------------------------------------- */
 
-typedef enum fn_actor_direction_e {
-  FN_ACTOR_DIRECTION_LEFT,
-  FN_ACTOR_DIRECTION_RIGHT
-} fn_actor_direction_e;
-
-/* --------------------------------------------------------------- */
-
 /**
  * The actor types.
  */
